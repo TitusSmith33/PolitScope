4ee74682a4bcd15ad679389303b2ea1688830224
TitusSmith33 <smith03@allegheny.edu>
2025-02-07 12:46:56 -0500

    update: updated function calls

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,12 +1,11 @@
-import json
 from embedding import generate_embeddings
-from text_processor import tokenize_and_clean
+from text_processor import preprocess_text
 
 
 data ={"content": "This is a random sentence. Here is another one. politcal bias lives all around us and Trump is crazy #biden2028"}
 
 def main():
-    clean_data = tokenize_and_clean(data)
+    clean_data = preprocess_text(data)
     example_output = generate_embeddings(clean_data)
     print(example_output)
     print(type(example_output))
