fdb58ad69cf22659ba4609e6501e5f0f9877011c
TitusSmith33 <smith03@allegheny.edu>
2025-02-13 20:13:20 -0500

    update: update main to handle clustering and testing

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,14 +1,18 @@
 from embedding import generate_embeddings
 from text_processor import preprocess_text
+from analysis import analyze_embeddings
 
 
-data ={"content": "This is a random sentence. Here is another one. politcal bias lives all around us and Trump is crazy #biden2028"}
+data ={"content": "Trump is my favorite president, and I really like that he won the election. this is a normal sentence with no impact. I am adding random sentences. this is just an example sentence. politcal bias lives all around us and Trump is crazy #biden2028. I think democrats are the smartest voters in the country. I think republicans and people that vote for trump are the smartest voters in the world."}
 
 def main():
     clean_data = preprocess_text(data)
     example_output = generate_embeddings(clean_data)
-    print(example_output)
-    print(type(example_output))
-    print("1")
+    #print(example_output)
+    #print(type(example_output))
+    #print("1")
+    num_clusters = 2
+    analyze_embeddings(example_output, num_clusters)
+
 
 main()
